6aad591f8dc630aee044dcfe698212f4d5e5a9de
nakyung128 <lnk12888@naver.com>
2022-02-03 17:21:46 +0900

    id clear!!!!!

--- app/src/main/java/com/example/guru2/GroupMaking.kt
+++ app/src/main/java/com/example/guru2/GroupMaking.kt
@@ -14,7 +14,7 @@ import android.widget.Toast
 class GroupMaking : AppCompatActivity() {
 
     lateinit var myHelper: myDBHelper
-    lateinit var myHelper2: myDBHelper
+    lateinit var myHelper2: myDBHelper2
     lateinit var edtName: EditText
     lateinit var btnGname: Button
     lateinit var edtNumber: EditText
@@ -42,7 +42,7 @@ class GroupMaking : AppCompatActivity() {
         // groupDB
         myHelper = myDBHelper(this)
         // personnelDB
-        myHelper2 = myDBHelper(this)
+        myHelper2 = myDBHelper2(this)
 
         // 그룹명 중복확인
         btnGname.setOnClickListener {
@@ -73,17 +73,17 @@ class GroupMaking : AppCompatActivity() {
                         edtNumber.text.toString() + ", '" + edtTextBox.text.toString() + "', '" + edtStep.text.toString() + "', " + 1.toString() + ", ' ', ' ', ' ', ' ');")
 
                 // 만든 사람 그룹 멤버로 추가하기
-                /*sqlDB = myHelper2.readableDatabase
+                sqlDB = myHelper2.readableDatabase
                 var cursor: Cursor
-                cursor = sqlDB.rawQuery("SELECT id FROM personnelDB", null) // 디비에서 해당 로그인 아이디 가져오기
+                cursor = sqlDB.rawQuery("SELECT id FROM personnel", null) // 디비에서 해당 로그인 아이디 가져오기
 
                 while (cursor.moveToNext()) {
                     id = cursor.getString(0) // id
                 }
-                // gMember1로 추가
+                // gMember1로 생성 아이디 추가하기
                 sqlgDB.execSQL("UPDATE groupDB SET gMember1 = '" + id + "' WHERE gName = '" + edtName.text.toString() + "';")
                 cursor.close()
-                sqlDB.close()*/
+                sqlDB.close()
                 sqlgDB.close()
                 Toast.makeText(applicationContext, "${edtName.text} 그룹이 생성되었습니다!", Toast.LENGTH_SHORT).show()
                 
@@ -91,10 +91,12 @@ class GroupMaking : AppCompatActivity() {
                 val intent = Intent(this, GroupShow::class.java)
                 startActivity(intent)
             } else {
+                // 그룹명 중복 아니지만 인원수 4 초과할 때
                 if (edtNumber.text.toString().toInt() > 4 && available) {
                     Toast.makeText(applicationContext, "인원수를 4명 이하로 설정해 주세요!", Toast.LENGTH_SHORT).show()
                 }
-                if (edtNumber.text.toString().toInt() <= 4 && !available) {
+                // 인원수가 4 이하이지만 그룹명이 중복일 때
+                else if (edtNumber.text.toString().toInt() <= 4 && !available) {
                     Toast.makeText(applicationContext, "그룹명을 바꿔 주세요.", Toast.LENGTH_SHORT).show()
                 }
             }
